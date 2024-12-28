1c0e709d9087b82f83d2f9ea84c675597f46a87d
1dhruvsingh <dhruvsinghsir@gmail.com>
2024-12-29 00:49:18 +0530

    DOM 1

--- Web-dev/DOM /codes/script.js
+++ Web-dev/DOM /codes/script.js
@@ -6,6 +6,8 @@ function addTodo() {
 
 let ctr= 0;
 function callback(){
-    console.log(ctr);
+    document.querySelectorAll("h4")[1].innerHTML =ctr;
+    ctr = ctr + 1;
 }
-setInterval
+
+setInterval (callback, 1000);
