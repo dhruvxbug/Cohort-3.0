5769e17b9e584880ccb368e2eb171c3296421b74
1dhruvsingh <dhruvsinghsir@gmail.com>
2024-12-29 14:52:27 +0530

    Debug complete

--- Web-dev/DOM /To-do-web/script.js
+++ Web-dev/DOM /To-do-web/script.js
@@ -1,17 +1,17 @@
-let n= 1;
+let n= 0;
     function addTodo(){
         const inputEl = document.querySelector("input");
         const value = inputEl.value;
         
         const newDivEl = document.createElement("div");
-        newDivEl.setAttribute("id","one-", n);
-        n = n + 1;
-        newDivEl.innerHTML = "<div>" + value +"</div> <button id= 'one- n' onclick='deleteitem(" + n + " )'> delete</button>";
+        newDivEl.setAttribute("id","one-"+ n);
+        newDivEl.innerHTML = "<div>" + value +'</div> <button onclick ="deleteitem(' + n + ' )"> delete</button>';
         document.querySelector ("body").appendChild(newDivEl);
+        n = n + 1;
     }
     
     
-    function deleteitem(){
-       const Element = document.getElementById("one-" + index);
-       Element.parentNode.removeChild(Element);
+    function deleteitem(index){
+       const element = document.getElementById("one-" + index);
+       element.parentNode.removeChild(element);
     }
