1c0685cb56a866dbec09bc8879df45883f3fa614
1dhruvsingh <dhruvsinghsir@gmail.com>
2025-01-27 14:40:24 +0530

    Hospital backend

--- Web-dev/Express/index.js
+++ Web-dev/Express/index.js
@@ -8,6 +8,7 @@ const users =[{
     }]
 }];
 
+// reads the data from the user
 app.get("/", function(req, res){
 
     const johnKidneys = users[0].kidneys;
@@ -28,6 +29,7 @@ app.get("/", function(req, res){
 
 app.use(express.json());
 
+// adds a new kidney 
 app.post("/", function(req, res){
     const isHealthy = req.body.isHealthy;
     users[0].kidneys.push({
@@ -37,5 +39,53 @@ app.post("/", function(req, res){
         msg:"Done!"
     })
 })
+
+//converts unhealthy kidney to healthy kidney 
+app.put("/", function(req, res){
+    if (isThereAtleastOneUnhealthyKidney()){
+        for (let i=0; i<users[0].kidneys.length; i++){
+            users[0].kidneys[i].healthy = true;
+        }
+        res.json({});
+    }
+    else{
+        res.status(411).json({
+            msg: "you have no bad kidneys to convert"
+        })
+    }
+})
+
+// removing all the unhealthy kidneys 
+app.delete("/", function(req, res){
+    // only run if there are unhealthy kidneys present 
+    if (isThereAtleastOneUnhealthyKidney()){
+        const newKidneys =[];
+        for (let i=0; i<users[0].kidneys.length; i++){
+            if (users[0].kidneys[i].healthy){
+                 newKidneys.push({
+                    healthy: true
+                 })
+            }
+        }
+        users[0].kidneys = newKidneys;
+        res.json({msg:"done"})
+    }
+    else{
+        res.status(411).json({
+            msg: "you have no bad kidneys"
+        });
+    }
+    
+})
+
+function isThereAtleastOneUnhealthyKidney(){
+    let atleastOneUnhealthyKidney = false;
+    for(let i=0; i<users[0].kidneys.length; i++){
+        if (!users[0].kidneys[i].healthy){
+            atleastOneUnhealthyKidney = true;
+        }
+    }
+    return atleastOneUnhealthyKidney
+}
 
 app.listen(3000);
