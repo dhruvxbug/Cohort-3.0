20bd25fc3bd280735c1d169596d4d17cbf84d31f
1dhruvsingh <142042993+1dhruvsingh@users.noreply.github.com>
2025-05-22 13:54:43 +0530

    react

---
+++ React/basics/src/App1.jsx
@@ -0,0 +1,46 @@
+import {useState, useEffect} from 'react';
+
+//useEffect, dependency array, cleanups 
+export default function App(){
+    const [count, setCount] = useState(0);
+    const [count2, setCount2] = useState(0);
+
+    function increase(){
+       setCount(c =>c +1);
+    }
+
+    function decrease(){
+       setCount(c =>c -1);
+    } 
+
+    return <div>
+        <Counter count={count} count2={count2}/>
+        <button onClick={increase}> Increase count </button>
+        <button onClick={decrease}> Decrease count </button>
+    </div>
+}
+
+function Counter(props){
+   
+
+    useEffect(function(){
+        console.log("mount");
+
+        return function(){
+            console.log("unmount");
+        }
+    }, []);
+
+    useEffect(function(){
+        console.log("count has changed");
+
+        return function(){
+            console.log("cleanup inside second effect")
+        }
+    }, [props.count , props.count2])
+
+    return <div>
+        Counter {props.count} <br>
+        Counter2{props.count2}</br>
+    </div>
+}
