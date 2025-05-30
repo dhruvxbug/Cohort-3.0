dc72d2220e477a7ab2a765c92c1a0417a2878e71
1dhruvsingh <142042993+1dhruvsingh@users.noreply.github.com>
2025-05-30 20:18:47 +0530

    routing

--- React/week-9/src/App.jsx
+++ React/week-9/src/App.jsx
@@ -1,6 +1,47 @@
-import React, {Fragment, useState, useEffect} from 'react'
+import {useState} from 'react'
+import {BrowserRouter, Routes, Route, Link, useNavigate} from "react-router-dom"
 
 export default function App(){
+  return (
+     <div>
+    <BrowserRouter>
+      <Link href="/">Allen</Link>
+      <Link href="/neet/class11">class 11</Link>
+      <Link href="/neet/class12">class 12</Link>
+      <Routes>
+        <Route path="/neet/class11" element={<Class11Program/>} />
+        <Route path="/neet/class12" element={<Class12Program/>} />
+        <Route path="/" element={<LandingPage/>} />
+      </Routes>
+    </BrowserRouter>
+     </div>
+  )
+}
+
+function Class11Program(){
   return(
+    <div>
+        NEET for class 11th 
+    </div>
+  )
+}
+function Class12Program(){
+  const navigate = useNavigate();
+
+  function redirectUser(){
+    navigate("/")
+  }
+  return(
+    <div>
+        NEET for class 12th 
+        <button onClick={redirectUser}> Go back to the landing page </button>
+    </div>
+  )
+}
+function LandingPage(){
+  return(
+    <div>
+        I am Main landing page 
+    </div>
   )
 }
